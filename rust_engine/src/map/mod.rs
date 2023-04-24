6bd880529add6424b7ee3163d7b63779f483c6e0
KDreynolds <kylereynoldsdev@gmail.com>
2023-04-23 23:33:29 -0600

    so much shit

--- rust_engine/src/map/mod.rs
+++ rust_engine/src/map/mod.rs
@@ -21,10 +21,32 @@ impl Dungeon {
     }
 
     fn generate_random_walk(&mut self) {
-        // ... (same as before)
+        let mut rng = rand::thread_rng();
+        let mut x = self.width / 2;
+        let mut y = self.height / 2;
+
+        for _ in 0..(self.width * self.height) / 4 {
+            self.tiles[x][y].is_wall = false;
+
+            let direction = rng.gen_range(0..4);
+            match direction {
+                0 if x > 0 => x -= 1,
+                1 if x < self.width - 1 => x += 1,
+                2 if y > 0 => y -= 1,
+                3 if y < self.height - 1 => y += 1,
+                _ => (),
+            }
+        }
     }
 
-    pub fn render(&self) {
-        // ... (same as before)
+
+fn render(&self) {
+    for y in 0..self.height {
+        for x in 0..self.width {
+            let tile_char = if self.tiles[x][y].is_wall { '#' } else { '.' };
+            print!("{}", tile_char);
+        }
+        println!();
     }
-}
+}}
+
